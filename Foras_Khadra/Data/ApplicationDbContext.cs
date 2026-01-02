082c0c9f295d86cef4e4ec1918b06991e8a9ce81
Eng.Aya AL_Awesat <133769823+aya2000-allan@users.noreply.github.com>
2026-01-02 22:42:24 +0200

    Opportunity

---
+++ Foras_Khadra/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Foras_Khadra.Models;
+
+namespace Foras_Khadra.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TeamMember> TeamMember { get; set; }
+        public DbSet<Article> Articles { get; set; }
+
+    }
+}
