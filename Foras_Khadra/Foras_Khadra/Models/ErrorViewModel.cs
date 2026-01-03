0960d4089d79c50647287a6ec48c892b437b8591
Eng.Aya AL_Awesat <133769823+aya2000-allan@users.noreply.github.com>
2026-01-03 07:35:23 +0200

    lastupdate

--- Foras_Khadra/Foras_Khadra/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Foras_Khadra.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
