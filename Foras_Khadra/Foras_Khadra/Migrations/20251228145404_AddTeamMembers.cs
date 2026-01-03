fff90b52de1e49274b7ca345576d6538e3d4fcf2
Eng.Aya AL_Awesat <133769823+aya2000-allan@users.noreply.github.com>
2026-01-03 08:15:35 +0200

    lastUpdate

---
+++ Foras_Khadra/Foras_Khadra/Migrations/20251228145404_AddTeamMembers.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Foras_Khadra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTeamMembers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TeamMembers",
+                table: "TeamMembers");
+
+            migrationBuilder.RenameTable(
+                name: "TeamMembers",
+                newName: "TeamMember");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TeamMember",
+                table: "TeamMember",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TeamMember",
+                table: "TeamMember");
+
+            migrationBuilder.RenameTable(
+                name: "TeamMember",
+                newName: "TeamMembers");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TeamMembers",
+                table: "TeamMembers",
+                column: "Id");
+        }
+    }
+}
---
+++ Foras_Khadra/Foras_Khadra/Models/AllOpportunitiesViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Foras_Khadra.Helpers;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+
+namespace Foras_Khadra.Models
+{
+    public class AllOpportunitiesViewModel
+    {
+        public List<Opportunity> Opportunities { get; set; } = new();
+
+        public string SelectedCountry { get; set; }
+        public OpportunityType? SelectedType { get; set; }
+
+        // ⚡ إضافة هذه الخاصية للقائمة المنسدلة
+        public List<string> Countries { get; set; } = new List<string>();
+
+        // لتوليد قائمة الأنواع للـ dropdown
+        public List<SelectListItem> TypeSelectList
+        {
+            get
+            {
+                var list = new List<SelectListItem>();
+                foreach (var t in Enum.GetValues(typeof(OpportunityType)))
+                {
+                    var type = (OpportunityType)t;
+                    list.Add(new SelectListItem
+                    {
+                        Text = type.GetDisplayName(),
+                        Value = type.ToString(),
+                        Selected = SelectedType.HasValue && SelectedType.Value == type
+                    });
+                }
+                return list;
+            }
+        }
+    }
+}
