9af3072efc3235cf01043d5109526c6b2dd258e4
Eng.Aya AL_Awesat <133769823+aya2000-allan@users.noreply.github.com>
2026-02-01 22:53:08 +0200

    Update_Login

--- Foras_Khadra/Foras_Khadra/Models/LoginViewModel.cs
+++ Foras_Khadra/Foras_Khadra/Models/LoginViewModel.cs
@@ -1,13 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
+using Foras_Khadra.Resources.Views.Account;
 namespace Foras_Khadra.Models
 {
     public class LoginViewModel
     {
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessageResourceType = typeof(LoginResources), ErrorMessageResourceName = "EmailRequired")]
+        [EmailAddress(ErrorMessageResourceType = typeof(LoginResources), ErrorMessageResourceName = "EmailInvalid")]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(LoginResources), ErrorMessageResourceName = "PasswordRequired")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
