98021003fd0b837a0181dd63bd2994802bea26e5
Eng.Aya AL_Awesat <133769823+aya2000-allan@users.noreply.github.com>
2026-02-02 16:10:15 +0200

    .

--- Foras_Khadra/Foras_Khadra/Models/ForgotPasswordViewModel.cs
+++ Foras_Khadra/Foras_Khadra/Models/ForgotPasswordViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
+using System.ComponentModel.DataAnnotations;
+using Foras_Khadra.Resources.Views.Account;
 namespace Foras_Khadra.Models
 {
     public class ForgotPasswordViewModel
     {
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessageResourceType = typeof(ForgotPasswordResources), ErrorMessageResourceName = "EmailRequired")]
+        [EmailAddress(ErrorMessageResourceType = typeof(ForgotPasswordResources), ErrorMessageResourceName = "EmailInvalid")]
         public string Email { get; set; }
     }
 }
