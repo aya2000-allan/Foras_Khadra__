2fc5d6f80e6ad50588a7aba7aa19fca50dcdccc8
Eng.Aya AL_Awesat <133769823+aya2000-allan@users.noreply.github.com>
2026-02-02 21:41:16 +0200

    .

--- Foras_Khadra/Foras_Khadra/Models/OrganizationResetPasswordViewModel.cs
+++ Foras_Khadra/Foras_Khadra/Models/OrganizationResetPasswordViewModel.cs
@@ -6,33 +6,33 @@ namespace Foras_Khadra.Models
     public class OrganizationResetPasswordViewModel
     {
         [Required(
-            ErrorMessageResourceType = typeof(ResetPasswordResources),
-            ErrorMessageResourceName = nameof(ResetPasswordResources.EmailRequired))]
+            ErrorMessageResourceType = typeof(ResetPasswordOrgResources),
+            ErrorMessageResourceName = nameof(ResetPasswordOrgResources.EmailRequired))]
         [EmailAddress(
-            ErrorMessageResourceType = typeof(ResetPasswordResources),
-            ErrorMessageResourceName = nameof(ResetPasswordResources.EmailInvalid))]
+            ErrorMessageResourceType = typeof(ResetPasswordOrgResources),
+            ErrorMessageResourceName = nameof(ResetPasswordOrgResources.EmailInvalid))]
         public string Email { get; set; }
         public string Token { get; set; }
 
         [Required(
-            ErrorMessageResourceType = typeof(ResetPasswordResources),
-            ErrorMessageResourceName = nameof(ResetPasswordResources.PasswordRequired))]
+            ErrorMessageResourceType = typeof(ResetPasswordOrgResources),
+            ErrorMessageResourceName = nameof(ResetPasswordOrgResources.PasswordRequired))]
         [DataType(DataType.Password)]
         [StringLength(100, MinimumLength = 8,
-            ErrorMessageResourceType = typeof(ResetPasswordResources),
-            ErrorMessageResourceName = nameof(ResetPasswordResources.PasswordLength))]
+            ErrorMessageResourceType = typeof(ResetPasswordOrgResources),
+            ErrorMessageResourceName = nameof(ResetPasswordOrgResources.PasswordLength))]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$",
-            ErrorMessageResourceType = typeof(ResetPasswordResources),
-            ErrorMessageResourceName = nameof(ResetPasswordResources.PasswordComplexity))]
+            ErrorMessageResourceType = typeof(ResetPasswordOrgResources),
+            ErrorMessageResourceName = nameof(ResetPasswordOrgResources.PasswordComplexity))]
         public string Password { get; set; }
 
         [Required(
-            ErrorMessageResourceType = typeof(ResetPasswordResources),
-            ErrorMessageResourceName = nameof(ResetPasswordResources.ConfirmPasswordRequired))]
+            ErrorMessageResourceType = typeof(ResetPasswordOrgResources),
+            ErrorMessageResourceName = nameof(ResetPasswordOrgResources.ConfirmPasswordRequired))]
         [DataType(DataType.Password)]
         [Compare("Password",
-            ErrorMessageResourceType = typeof(ResetPasswordResources),
-            ErrorMessageResourceName = nameof(ResetPasswordResources.PasswordMismatch))]
+            ErrorMessageResourceType = typeof(ResetPasswordOrgResources),
+            ErrorMessageResourceName = nameof(ResetPasswordOrgResources.PasswordMismatch))]
         public string ConfirmPassword { get; set; }
     }
 
